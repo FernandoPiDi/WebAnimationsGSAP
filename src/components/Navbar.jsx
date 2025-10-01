98a721497a61f6d8f4374d9433a92a548e628629
Fernando Pinto <duvanpidi@hotmail.com>
2025-09-30 23:24:53 -0500

    feat: creation constants file and public directory

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import { navLinks } from '../../constants'
+import { useGSAP } from '@gsap/react'
+import { gsap } from 'gsap'
+
 
 const Navbar = () => {
+    useGSAP(() => {
+        const navTween = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'nav',
+                start: 'bottom top',
+            }
+        })
+    })
+
   return (
     <nav>
         <div>
             <a href='#home' className='flex items-center gap-2'> 
+                <img src="/images/logo.png" alt="logo" className='w-10 h-10' />
                 <p>Velvet Pour</p> 
             </a>
             <ul>
