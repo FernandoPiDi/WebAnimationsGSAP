71598022bed0ddc04223641b98796ff0c792c33b
Fernando Pinto <duvanpidi@hotmail.com>
2025-10-02 23:50:35 -0500

    Modifico App.jsx y agrego el componente Cocktails

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-
+import Cocktails from './components/Cocktails';
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 
@@ -12,6 +12,7 @@ return (
   <main>
     <Navbar />
     <Hero />
+    <Cocktails />
   </main>
 )
 };
